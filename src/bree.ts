6eee8e26e8a6747c064b32a8b5f240dfd50e7453
rohit <rohitchandan998@gmail.com>
2023-09-10 16:50:05 +0530

    update

---
+++ src/bree.ts
@@ -0,0 +1,18 @@
+import Bree from 'bree';
+import Graceful from '@ladjs/graceful';
+
+const bree = new Bree({
+  jobs: [
+    {
+      name: 'job',
+      interval: '10s',
+    },
+  ],
+});
+
+// handle graceful reloads, pm2 support, and events like SIGHUP, SIGINT, etc.
+const graceful = new Graceful({ brees: [bree] });
+graceful.listen();
+
+// start all jobs (this is the equivalent of reloading a crontab):
+bree.start();
