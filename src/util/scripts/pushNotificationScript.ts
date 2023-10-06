0f7462374d61fe3892aa80586656895fa7f8bf79
rohit <rohitchandan998@gmail.com>
2023-10-06 18:20:32 +0530

    udpate

--- src/util/scripts/pushNotificationScript.ts
+++ src/util/scripts/pushNotificationScript.ts
@@ -25,14 +25,139 @@ function askForNotificationPermission() {
     Notification.requestPermission().then(async (permission) => {
       if (permission === 'granted') {
         try {
-          console.log('loadddd', navigator?.serviceWorker);
-          const registration = await navigator.serviceWorker.register(
-            '/serviceWorker.js',
-          );
+          await navigator.serviceWorker.register('/serviceWorker.js');
+
+          const sw = await navigator.serviceWorker.ready;
+
+          const subExisted = await sw.pushManager.getSubscription();
+          if (subExisted) {
+            if (successUrl) window.location.href = successUrl;
+            return true;
+          }
+
+          const pushSubscription = await sw.pushManager.subscribe({
+            userVisibleOnly: true,
+            applicationServerKey: PUSH_DATA?.VAPID_PUBLIC_KEY,
+          });
+
+          // Parse the query string from the URL
+          const url = new URL(window.location.href);
+          const queryParams = new URLSearchParams(url.search);
+          const lang = navigator.language || navigator.userLanguage;
+          const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+          const { userAgent } = navigator;
+
+          // geting device type
+          let browser = '';
+          if (
+            userAgent.includes('Opera')
+            || userAgent.includes('OPR')
+            || userAgent.includes('Opr')
+          ) {
+            browser = 'Opera';
+          } else if (userAgent.includes('Edg')) {
+            browser = 'Edge';
+          } else if (userAgent.includes('Chrome')) {
+            browser = 'Chrome';
+          } else if (userAgent.includes('Safari')) {
+            browser = 'Safari';
+          } else if (userAgent.includes('Firefox')) {
+            browser = 'Firefox';
+          } else if (userAgent.includes('Mozilla')) {
+            browser = 'Mozilla';
+          } else if (userAgent.includes('Samsung')) {
+            browser = 'Samsung';
+          } else if (userAgent.includes('Android')) {
+            browser = 'Android';
+          } else if (userAgent.includes('UCBrowser')) {
+            browser = 'UCBrowser';
+          } else {
+            browser = 'Unknown';
+          }
+
           console.log(
-            'Service Worker registered with scope:',
-            registration.scope,
+            'user agent',
+            userAgent,
+            browser,
+            navigator?.userAgentData?.platform,
+            navigator,
           );
+
+          // getting device type
+          const userAgentLowerCase = userAgent.toLowerCase();
+          const isMobile = /iphone|ipod|android|blackberry|opera mini|iemobile|wpdesktop/i.test(
+            userAgentLowerCase,
+          );
+          const isTablet = /ipad|android|kindle/i.test(userAgentLowerCase);
+          const isSmarttv = /smarttv|appletv|googletv|hbbtv|pov_tv|netcast|webos|roku|dlnadoc|roku|viera|boxee|kylo|yabrowser|kaltura/i.test(
+            userAgentLowerCase,
+          );
+          const isConsole = /xbox|playstation/i.test(userAgentLowerCase);
+          const isWearable = /android|ipod|iphone|symbian|blackberry|smartwatch/i.test(
+            userAgentLowerCase,
+          );
+          const isEmbedded = /automotive|phantom/i.test(userAgentLowerCase);
+
+          let deviceType = 'Desktop'; // Default to Desktop
+          if (isMobile) {
+            deviceType = 'Mobile';
+          } else if (isTablet) {
+            deviceType = 'Tablet';
+          } else if (isSmarttv) {
+            deviceType = 'Smarttv';
+          } else if (isConsole) {
+            deviceType = 'Console';
+          } else if (isWearable) {
+            deviceType = 'Wearable';
+          } else if (isEmbedded) {
+            deviceType = 'Embedded';
+          }
+
+          let os = navigator?.userAgentData?.platform;
+          if (!os) {
+            if (/Windows/.test(userAgent)) {
+              os = 'Windows';
+            } else if (/Macintosh|Mac OS/.test(userAgent)) {
+              os = 'MacOS';
+            } else if (/Linux/.test(userAgent)) {
+              os = 'Linux (Unix-like)';
+            } else if (/Android/.test(userAgent)) {
+              os = 'Android';
+            } else if (/iOS|iPhone|iPad/.test(userAgent)) {
+              os = 'IOS';
+            } else {
+              os = 'Unknown';
+            }
+          }
+
+          const requestBody: any = {
+            feedId,
+            lang,
+            os,
+            timezone,
+            browser,
+            deviceType,
+            pushSubscription,
+          };
+
+          // Iterate through all query parameters
+          for (const [paramName, paramValue] of queryParams.entries()) {
+            // Add each parameter to the request body
+            requestBody[paramName] = paramValue;
+          }
+
+          if (postbackUrl) requestBody.postbackUrl = postbackUrl;
+
+          // sub logic here
+          await fetch(`${PUSH_DATA?.SUBSCRIPTION_URL as string}`, {
+            method: 'POST', // Specify the HTTP method
+            headers: {
+              'Content-Type': 'application/json', // Specify the content type
+            },
+            body: JSON.stringify(requestBody), // Convert the body object to JSON
+          });
+          // redirect logic here
+          if (successUrl) window.location.href = successUrl;
         } catch (error) {
           console.error(
             'Service Worker registration failed:',
@@ -40,139 +165,6 @@ function askForNotificationPermission() {
             error?.message,
           );
         }
-        console.log('server sw', await navigator.serviceWorker.ready);
-
-        const sw = await navigator.serviceWorker.ready;
-        console.log('server sw', sw);
-        const pushSubscription = await sw.pushManager.subscribe({
-          userVisibleOnly: true,
-          applicationServerKey: PUSH_DATA?.VAPID_PUBLIC_KEY,
-        });
-        console.log(JSON.stringify(pushSubscription));
-
-        // Parse the query string from the URL
-        const url = new URL(window.location.href);
-        const queryParams = new URLSearchParams(url.search);
-        const lang = navigator.language || navigator.userLanguage;
-        const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-        const { userAgent } = navigator;
-
-        // geting device type
-        let browser = '';
-        if (
-          userAgent.includes('Opera')
-          || userAgent.includes('OPR')
-          || userAgent.includes('Opr')
-        ) {
-          browser = 'Opera';
-        } else if (userAgent.includes('Edg')) {
-          browser = 'Edge';
-        } else if (userAgent.includes('Chrome')) {
-          browser = 'Chrome';
-        } else if (userAgent.includes('Safari')) {
-          browser = 'Safari';
-        } else if (userAgent.includes('Firefox')) {
-          browser = 'Firefox';
-        } else if (userAgent.includes('Mozilla')) {
-          browser = 'Mozilla';
-        } else if (userAgent.includes('Samsung')) {
-          browser = 'Samsung';
-        } else if (userAgent.includes('Android')) {
-          browser = 'Android';
-        } else if (userAgent.includes('UCBrowser')) {
-          browser = 'UCBrowser';
-        } else {
-          browser = 'Unknown';
-        }
-
-        console.log(
-          'user agent',
-          userAgent,
-          browser,
-          navigator?.userAgentData?.platform,
-          navigator,
-        );
-
-        // getting device type
-        const userAgentLowerCase = userAgent.toLowerCase();
-        const isMobile = /iphone|ipod|android|blackberry|opera mini|iemobile|wpdesktop/i.test(
-          userAgentLowerCase,
-        );
-        const isTablet = /ipad|android|kindle/i.test(userAgentLowerCase);
-        const isSmarttv = /smarttv|appletv|googletv|hbbtv|pov_tv|netcast|webos|roku|dlnadoc|roku|viera|boxee|kylo|yabrowser|kaltura/i.test(
-          userAgentLowerCase,
-        );
-        const isConsole = /xbox|playstation/i.test(userAgentLowerCase);
-        const isWearable = /android|ipod|iphone|symbian|blackberry|smartwatch/i.test(
-          userAgentLowerCase,
-        );
-        const isEmbedded = /automotive|phantom/i.test(userAgentLowerCase);
-
-        let deviceType = 'Desktop'; // Default to Desktop
-        if (isMobile) {
-          deviceType = 'Mobile';
-        } else if (isTablet) {
-          deviceType = 'Tablet';
-        } else if (isSmarttv) {
-          deviceType = 'Smarttv';
-        } else if (isConsole) {
-          deviceType = 'Console';
-        } else if (isWearable) {
-          deviceType = 'Wearable';
-        } else if (isEmbedded) {
-          deviceType = 'Embedded';
-        }
-
-        let os = navigator?.userAgentData?.platform;
-        if (!os) {
-          if (/Windows/.test(userAgent)) {
-            os = 'Windows';
-          } else if (/Macintosh|Mac OS/.test(userAgent)) {
-            os = 'MacOS';
-          } else if (/Linux/.test(userAgent)) {
-            os = 'Linux (Unix-like)';
-          } else if (/Android/.test(userAgent)) {
-            os = 'Android';
-          } else if (/iOS|iPhone|iPad/.test(userAgent)) {
-            os = 'IOS';
-          } else {
-            os = 'Unknown';
-          }
-        }
-
-        const requestBody: any = {
-          feedId,
-          lang,
-          os,
-          timezone,
-          browser,
-          deviceType,
-          pushSubscription,
-        };
-
-        // Iterate through all query parameters
-        for (const [paramName, paramValue] of queryParams.entries()) {
-          // Add each parameter to the request body
-          requestBody[paramName] = paramValue;
-        }
-
-        if (postbackUrl) requestBody.postbackUrl = postbackUrl;
-
-        // sub logic here
-        fetch(`${PUSH_DATA?.SUBSCRIPTION_URL as string}`, {
-          method: 'POST', // Specify the HTTP method
-          headers: {
-            'Content-Type': 'application/json', // Specify the content type
-          },
-          body: JSON.stringify(requestBody), // Convert the body object to JSON
-        })
-          .then(() => {
-            // redirect logic here
-            if (successUrl) window.location.href = successUrl;
-          })
-          .catch((er) => {
-            console.log(er);
-          });
       } else if (permission === 'denied') {
         const url = new URL(window.location.href);
         const queryParams = new URLSearchParams(url.search) as any;
